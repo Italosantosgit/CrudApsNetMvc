62322a8c9fa423d0a2d2ca575b477db7db3ec80e
Italo Santos <itallo_@Outlook.com>
2018-03-18 17:50:31 -0300

    commit do crudAspPaginacao

---
+++ CrudAspNetMvc/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using CrudAspNetMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CadastroContext Cc = new CadastroContext();
+        }
+    }
+}
---
+++ CrudAspNetMvc/CrudAspNetMvc/Models/CadastroContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CrudAspNetMvc.Models
+{
+    public class CadastroContext:DbContext
+    {
+        public CadastroContext():base("CadastroDB")
+        {
+            Database.SetInitializer<CadastroContext>(
+                new CreateDatabaseIfNotExists<CadastroContext>());
+            Database.Initialize(false);
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
